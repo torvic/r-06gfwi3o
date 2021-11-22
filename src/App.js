1c7467281e017ecb32003d0714dca6d2715fc4ce
torvic <130111@unsaac.edu.pe>
2021-11-22 17:04:57 -0500

    Feat: First component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Welcome from './Welcome';
 
 class App extends Component {
   constructor() {
@@ -13,8 +14,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        {this.state.names.map((name) =>
-          <Welcome name={name} />
+        {this.state.names.map((name, index) =>
+          <Welcome key={index} name={name} />
         )}
       </div>
     );
---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Welcome = ({ name }) => {
+	return (
+		<div>
+			<h1>Hola {name}</h1>
+		</div>
+	)
+}
+
+export default Welcome
